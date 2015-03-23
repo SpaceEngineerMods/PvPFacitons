182a57c26959eb3cc9397ee74fe21af1964b9542
Lethegrin <zackavino@gmail.com>
2015-03-23 10:15:11 +0800

    uploading

---
+++ PvPFactions/WeaponsRangeCheck.cs
@@ -0,0 +1,110 @@
+﻿using Sandbox.ModAPI;
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Common.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+<<<<<<< HEAD
+using Sandbox.ModAPI.Ingame;
+
+namespace WeaponsRangeCheck
+=======
+
+namespace ConsoleApplication2
+>>>>>>> c04a03317a2fcba94cf23c648edab11be5289963
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Beacon))]
+    public class MyBeaconLogic : MyGameLogicComponent
+    {
+        Sandbox.Common.ObjectBuilders.MyObjectBuilder_EntityBase m_objectBuilder = null;
+        private bool m_greeted;
+        public override void Close()
+        {
+        }
+
+        public override void Init(Sandbox.Common.ObjectBuilders.MyObjectBuilder_EntityBase objectBuilder)
+        {
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
+            m_objectBuilder = objectBuilder;
+        }
+
+        public override void MarkForClose()
+        {
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+        }
+
+        public override void UpdateAfterSimulation10()
+        {
+        }
+
+        public override void UpdateAfterSimulation100()
+        {
+        }
+
+        public override void UpdateBeforeSimulation()
+        {
+        }
+
+        public override void UpdateBeforeSimulation10()
+        {
+        }
+
+        public override void UpdateBeforeSimulation100()
+        {
+
+
+
+            HashSet<IMyEntity> workingSmallGatlingGuns = new HashSet<IMyEntity>();
+<<<<<<< HEAD
+            HashSet<IMyEntity> IMySmallGatlingGun;
+            Sandbox.ModAPI.MyAPIGateway.Entities.GetEntities(IMySmallGatlingGun,
+                (x) => x is IMySmallGatlingGun && x.IsWorking);
+=======
+            Sandbox.ModAPI.MyAPIGateway.Entities.GetEntities(workingSmallGatlingGuns, (x) => x is IMySmallGatlingGun() && x.IsWorking);
+>>>>>>> c04a03317a2fcba94cf23c648edab11be5289963
+
+
+            foreach (var SmallGatlingGun in workingSmallGatlingGuns)
+            {
+<<<<<<< HEAD
+                if (((Block.GetTopMostParent().EntityId() != SmallGatlingGun.GetTopMostParent().EntityId())) && (MyAPIGateway.Session.Player.GetPosition() - Entity.GetPosition()).Length() < 20)
+=======
+                if (((Block.GetTopMostParent().entityid() != SmallGatlingGun.GetTopMostParent().entityid())) && (MyAPIGateway.Session.Player.GetPosition() - Entity.GetPosition()).Length() < 20)
+>>>>>>> c04a03317a2fcba94cf23c648edab11be5289963
+                {
+                    if (!m_greeted)
+                    {
+                        MyAPIGateway.Utilities.ShowNotification(string.Format("Hello I am {0}", (Entity as Sandbox.ModAPI.Ingame.IMyTerminalBlock).DisplayNameText), 1000, MyFontEnum.Red);
+                        m_greeted = true;
+                    }
+                }
+                else
+                    m_greeted = false;
+            }
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+        }
+
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+        {
+            return m_objectBuilder;
+        }
+    }
+<<<<<<< HEAD
+
+    public class Block
+    {
+        public static void GetTopMostParent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+=======
+>>>>>>> c04a03317a2fcba94cf23c648edab11be5289963
+}
