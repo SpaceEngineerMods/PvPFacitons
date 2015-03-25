aa2645d363913aa6a729d46b00351dbf926f7573
Lethegrin <zackavino@gmail.com>
2015-03-25 09:34:20 +0800

    Works on listen servers but not dedicated.

--- WeaponsRangeCheck.cs
+++ WeaponsRangeCheck.cs
@@ -1,4 +1,4 @@
-﻿using Sandbox.ModAPI;
+using Sandbox.ModAPI;
 using Sandbox.Common;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Common.Components;
@@ -7,21 +7,21 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Text.RegularExpressions;
-
 using Sandbox.ModAPI.Ingame;
 
 namespace WeaponsRangeCheck
 {
-    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Beacon))]
+    [MyEntityComponentDescriptor(typeof (MyObjectBuilder_Beacon))]
     public class MyBeaconLogic : MyGameLogicComponent
     {
-        MyObjectBuilder_EntityBase m_objectBuilder;
+        private Sandbox.Common.ObjectBuilders.MyObjectBuilder_EntityBase m_objectBuilder = null;
         private bool m_greeted;
+
         public override void Close()
         {
         }
 
-        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        public override void Init(Sandbox.Common.ObjectBuilders.MyObjectBuilder_EntityBase objectBuilder)
         {
             Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
             m_objectBuilder = objectBuilder;
@@ -58,12 +58,13 @@ namespace WeaponsRangeCheck
 
             HashSet<IMyEntity> ships = new HashSet<IMyEntity>();
             //find all of the ships
-            MyAPIGateway.Entities.GetEntities(ships, x => x is Sandbox.ModAPI.IMyCubeGrid);
-        
+            Sandbox.ModAPI.MyAPIGateway.Entities.GetEntities(ships, (x) => x is Sandbox.ModAPI.IMyCubeGrid);
+
             var GatlingGuns = new List<IMySmallGatlingGun>();
             // go through ships
             foreach (var ship in ships)
             {
+              
 
                 var templist = new List<Sandbox.ModAPI.IMySlimBlock>();
                 // find all gatling guns in the group
@@ -76,35 +77,35 @@ namespace WeaponsRangeCheck
                     GatlingGuns.Add(temp.FatBlock as IMySmallGatlingGun);
                 }
             }
-    
-    
+
+
             foreach (var GatlingGun in GatlingGuns)
             {
-
 
                 var tempattackplayerid = GatlingGun.OwnerId;
                 var tempdefendplayerid = (Entity as Sandbox.ModAPI.IMyTerminalBlock).OwnerId;
                 var tempattackfactionid = MyAPIGateway.Session.Factions.TryGetPlayerFaction(tempattackplayerid);
                 var tempdefendfactionid = MyAPIGateway.Session.Factions.TryGetPlayerFaction(tempdefendplayerid);
-            
 
-                if (((Entity.GetTopMostParent().EntityId != GatlingGun.GetTopMostParent().EntityId)) && (GatlingGun.Enabled) && (GatlingGun.OwnerId != (Entity as Sandbox.ModAPI.IMyTerminalBlock).OwnerId) && (VRageMath.ContainmentType.Contains == GatlingGun.GetTopMostParent().WorldAABB.Contains(MyAPIGateway.Session.Player.GetPosition())) && (GatlingGun.GetPosition() - Entity.GetPosition()).Length() < 20)
+                if (
+                    Entity.GetTopMostParent().EntityId != GatlingGun.GetTopMostParent().EntityId &&
+                   GatlingGun.Enabled &&
+                    GatlingGun.OwnerId != (Entity as Sandbox.ModAPI.IMyTerminalBlock).OwnerId &&
+                    VRageMath.ContainmentType.Contains == GatlingGun.GetTopMostParent().WorldAABB.Contains(
+                        MyAPIGateway.Session.Player.GetPosition()
+                        ) &&
+                    (GatlingGun.GetPosition() - Entity.GetPosition()).Length() < 20
+                    )
                 {
-
-                    if (!m_greeted)
-                    {
-                    
-                        MyAPIGateway.Utilities.ShowNotification("GatlingGuns Detected", 1000, MyFontEnum.BuildInfoHighlight);
-                        MyAPIGateway.Session.Factions.DeclareWar(tempdefendfactionid.FactionId, tempattackfactionid.FactionId);
-                        m_greeted = true;
-
-                    }
+                    MyAPIGateway.Session.Factions.DeclareWar(tempdefendfactionid.FactionId, tempattackfactionid.FactionId);
+                    MyAPIGateway.Utilities.ShowNotification("Contraband Detected", 1000, MyFontEnum.BuildInfoHighlight);
                 }
-                else
-                    m_greeted = false;
+
+
+
             }
-        
         }
+
 
         public override void UpdateOnceBeforeFrame()
         {
