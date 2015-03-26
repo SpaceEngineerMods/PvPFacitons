6e3849bf02d17b35985e99aa221cb67e153f8235
AgentAlexGit <alex19985@gmail.com>
2015-03-26 01:24:18 -0400

    Fixed your things

--- PvPFactionsMod/WeaponsRangeCheck.cs
+++ PvPFactionsMod/WeaponsRangeCheck.cs
@@ -54,62 +54,61 @@ namespace WeaponsRangeCheck
         public override void UpdateBeforeSimulation100()
         {
 
-            HashSet<IMyEntity> ships = new HashSet<IMyEntity>();
+            HashSet<IMyEntity> infractingships = new HashSet<IMyEntity>();
+            var station = Entity.GetTopMostParent();
             //find all of the ships
-            Sandbox.ModAPI.MyAPIGateway.Entities.GetEntities(ships, (x) => x is Sandbox.ModAPI.IMyCubeGrid);
+            var gatlingGuns = new HashSet<IMyEntity>();
+            MyAPIGateway.Entities.GetEntities(gatlingGuns, (x) => x is IMySmallGatlingGun && (x.GetTopMostParent().GetPosition() - station.GetPosition()).Length() < 1000);
 
-            var GatlingGuns = new List<IMySmallGatlingGun>();
+            
             // go through ships
-            foreach (var ship in ships)
+         
+
+            foreach (var gatlingGun in gatlingGuns.Select(gatling => gatling as IMySmallGatlingGun))
             {
-
-
-                var templist = new List<Sandbox.ModAPI.IMySlimBlock>();
-                // find all gatling guns in the group
-                (ship as Sandbox.ModAPI.IMyCubeGrid).GetBlocks(templist,
-                    x => x.FatBlock is IMySmallGatlingGun);
-
-                foreach (var temp in templist)
-                {
-
-                    GatlingGuns.Add(temp.FatBlock as IMySmallGatlingGun);
-                }
-            }
-
-
-            foreach (var GatlingGun in GatlingGuns)
-            {
-
-                var tempattackplayerid = GatlingGun.OwnerId;
-                var tempdefendplayerid = (Entity as Sandbox.ModAPI.IMyTerminalBlock).OwnerId;
-                var tempattackfactionid = MyAPIGateway.Session.Factions.TryGetPlayerFaction(tempattackplayerid);
-                var tempdefendfactionid = MyAPIGateway.Session.Factions.TryGetPlayerFaction(tempdefendplayerid);
-
-
                 try
                 {
-                    if (Entity.GetTopMostParent().EntityId != GatlingGun.GetTopMostParent().EntityId &&
-                       (GatlingGun.Enabled) &&
-                        GatlingGun.OwnerId != (Entity as Sandbox.ModAPI.IMyTerminalBlock).OwnerId &&
-                        VRageMath.ContainmentType.Contains == GatlingGun.GetTopMostParent().WorldAABB.Contains(MyAPIGateway.Session.Player.GetPosition()) &&
-                        (GatlingGun.GetPosition() - Entity.GetPosition()).Length() < 1000)
+                    if (Entity.GetTopMostParent().EntityId != gatlingGun.GetTopMostParent().EntityId &&
+                        (gatlingGun.Enabled) &&
+                        gatlingGun.OwnerId != (Entity as Sandbox.ModAPI.IMyTerminalBlock).OwnerId)
+                        
                     {
+                        infractingships.Add(gatlingGun.GetTopMostParent());
 
-                        MyAPIGateway.Session.Factions.DeclareWar(tempdefendfactionid.FactionId, tempattackfactionid.FactionId);
-                        MyAPIGateway.Utilities.ShowNotification("You have failed to unpower your weapons", 1000, MyFontEnum.BuildInfoHighlight);
                     }
-                   else if((GatlingGun.GetPosition() - Entity.GetPosition()).Length() > 1000)
-                        {
-                          MyAPIGateway.Session.Factions.AcceptPeace(tempdefendfactionid.FactionId, tempattackfactionid.FactionId);
-                             MyAPIGateway.Utilities.ShowNotification("You have left our space", 1000, MyFontEnum.BuildInfoHighlight);
-                        }
+
 
                 }
                 catch
                 {
                     return;
                 }
+            }
 
+            foreach (var ship in infractingships)
+            {
+                try
+                {
+                    var tempattackplayerid = (ship as Sandbox.ModAPI.IMyCubeGrid).BigOwners[0];
+                    var tempdefendplayerid = (Entity as Sandbox.ModAPI.IMyTerminalBlock).OwnerId;
+                    var tempattackfactionid = MyAPIGateway.Session.Factions.TryGetPlayerFaction(tempattackplayerid);
+                    var tempdefendfactionid = MyAPIGateway.Session.Factions.TryGetPlayerFaction(tempdefendplayerid);
+
+                    if (VRageMath.ContainmentType.Contains ==
+                        ship.WorldAABB.Contains(MyAPIGateway.Session.Player.GetPosition()))
+                    {
+                        MyAPIGateway.Session.Factions.DeclareWar(tempdefendfactionid.FactionId,
+                            tempattackfactionid.FactionId);
+                        MyAPIGateway.Utilities.ShowNotification("You have failed to unpower your weapons", 1000,
+                            MyFontEnum.BuildInfoHighlight);
+                    }
+                }
+                catch 
+                {
+                    MyAPIGateway.Utilities.ShowNotification("We have detected you have no ownership data, you shall soon be detained.", 1000, MyFontEnum.BuildInfoHighlight);
+                   
+                }
+                
             }
 
         }
